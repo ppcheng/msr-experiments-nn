7304e23c088dd9caf4069fe3da3e844a3503d9c5
Josh Cheng <ai.ppcheng@gmail.com>
2017-07-26 10:59:38 -0400

    Initialize the repo

---
+++ README.md
@@ -0,0 +1,9 @@
+## tachyon
+- Can work on tensorflow v1+
+- Checkout `spn_example.py` for a very basic example on continuous case
+
+## CNN For NLP
+http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow
+
+http://www.wildml.com/2015/11/understanding-convolutional-neural-networks-for-nlp
+
---
+++ spn_example.py
@@ -0,0 +1,43 @@
+from tachyon.SPN2 import SPN
+
+# make an SPN holder
+spn = SPN()
+# include training and testing data
+spn.add_data('data/abalone/abalone.ts.data', 'train', cont=True)
+spn.add_data('data/abalone/abalone.valid.data', 'valid', cont=True)
+spn.add_data('data/abalone/abalone.test.data', 'test', cont=True)
+# create a valid sum product network
+
+sum_branch_factor = (3, 6)
+prod_branch_factor = (2, 4)
+variables = 8
+
+#continuous model
+spn.make_random_model((prod_branch_factor, sum_branch_factor), variables, cont=True, classify=False, data=spn.data.train)
+
+# start the session
+spn.start_session()
+
+# train
+
+# pick the optimization algorithm
+cccp = False
+gd = True
+count = True #take a step of counting then a step of gradient descent
+
+#large minibatches with a small number at a time
+minibatch_size=100
+
+# other stuff
+epochs = 1
+
+# access the data
+train = spn.data.train
+valid = spn.data.valid
+test = spn.data.test
+
+
+spn.train(epochs, train, cccp=cccp, gd=gd, count=count, minibatch_size=minibatch_size)
+test_loss = spn.evaluate(test, minibatch_size=1)
+print 'Loss:', test_loss
+# Loss: 4.513
